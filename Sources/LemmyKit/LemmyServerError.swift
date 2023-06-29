794884a12fbf64bc73499564b10759e62f32a5e5
Denis Dzyubenko <denis@ddenis.info>
2023-06-29 09:19:17 +0300

    Added structured enum for Lemmy error response codes

--- Sources/LemmyKit/LemmyServerError.swift
+++ Sources/LemmyKit/LemmyServerError.swift
@@ -6,6 +6,23 @@
 
 import Foundation
 
-public struct LemmyServerError: Error {
-    let message: String
+public enum LemmyServerError {
+    public enum LemmyServerErrorCodes: String {
+        /// The specified username or email was not accepted by the Lemmy server.
+        case couldnt_find_that_username_or_email
+
+        // The specified password was not accepted by the Lemmy server.
+        case password_incorrect
+    }
+
+    case value(LemmyServerErrorCodes)
+    case unknown(rawValue: String)
+
+    public init(from stringValue: String) {
+        if let value = LemmyServerErrorCodes(rawValue: stringValue) {
+            self = .value(value)
+        } else {
+            self = .unknown(rawValue: stringValue)
+        }
+    }
 }
