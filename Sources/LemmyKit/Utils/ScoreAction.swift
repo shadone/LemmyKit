15ad9c5a80b0b298bcb8dd90e5fdbda87dbcd7a1
Denis Dzyubenko <denis@ddenis.info>
2023-07-30 20:53:50 +0200

    Add CustomStringConvertible protocol to ScoreAction to make it loggable

--- Sources/LemmyKit/Utils/ScoreAction.swift
+++ Sources/LemmyKit/Utils/ScoreAction.swift
@@ -6,8 +6,16 @@
 
 import Foundation
 
-public enum ScoreAction: Int16, Encodable {
+public enum ScoreAction: Int16, Encodable, CustomStringConvertible {
     case upvote = 1
     case downvote = -1
     case unvote = 0
+
+    public var description: String {
+        switch self {
+        case .upvote:   return "upvote"
+        case .downvote: return "downvote"
+        case .unvote:   return "unvote"
+        }
+    }
 }
