d7d0457a8512984a77209515b918ee2ffd530530
Denis Dzyubenko <denis@ddenis.info>
2023-08-05 19:16:27 +0200

    Added api docs to Community

--- Sources/LemmyKit/Types/Community.swift
+++ Sources/LemmyKit/Types/Community.swift
@@ -9,11 +9,17 @@ import Foundation
 // Source: https://github.com/LemmyNet/lemmy/blob/main/crates/db_schema/src/source/community.rs
 
 public struct Community: Decodable {
+    /// Community identifier. The identifier is local to this instance.
     public let id: CommunityId
 
+    /// The unique name of the community.
+    ///
+    /// E.g. `mylittlepony`
     public let name: String
 
     /// A longer title, that can contain other characters, and doesn't have to be unique.
+    ///
+    /// E.g. `MLP: Friendship is Magic Reddit Community`
     public let title: String
 
     /// A sidebar / markdown description.
@@ -22,17 +28,21 @@ public struct Community: Decodable {
     /// Whether the community is removed by a mod.
     public let removed: Bool
 
+    /// The date community was created.
     public let published: Date
 
+    /// The date community info was last updated.
     public let updated: Date?
 
     /// Whether the community has been deleted by its creator.
     public let deleted: Bool
 
-    /// Whether its an NSFW community.
+    /// Whether it is an NSFW community.
     public let nsfw: Bool
 
     /// The federated actor_id.
+    ///
+    /// E.g. `https://lemmit.online/c/mylittlepony`
     public let actor_id: URL
 
     /// Whether the community is local.
