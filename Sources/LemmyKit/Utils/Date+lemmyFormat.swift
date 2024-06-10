172ecb15e9682d808d483938f08480b070b99cee
Denis Dzyubenko <denis@ddenis.info>
2024-06-10 23:25:49 +0200

    Fixed date parsing in Lemmy 0.19.0 responses

--- Sources/LemmyKit/Utils/Date+lemmyFormat.swift
+++ Sources/LemmyKit/Utils/Date+lemmyFormat.swift
@@ -14,6 +14,19 @@ extension Date {
 
         let utc = TimeZone(abbreviation: "UTC")!
 
+        // Lemmy 0.19.0 format: 2024-06-09T11:54:37.981990Z
+        let parseStrategyWithNanosecAndZ = Date.ParseStrategy(
+            format: "\(year: .defaultDigits)-\(month: .twoDigits)-\(day: .twoDigits)T\(hour: .defaultDigits(clock: .twentyFourHour, hourCycle: .zeroBased)):\(minute: .twoDigits):\(second: .twoDigits).\(secondFraction: .fractional(6))Z",
+            locale: Locale(identifier: "C"),
+            timeZone: utc
+        )
+
+        if let date = try? Date(stringValue, strategy: parseStrategyWithNanosecAndZ) {
+            self = date
+            return
+        }
+
+        // Lemmy 0.18 format: 2023-06-17T09:22:01.168808
         let parseStrategyWithNanosec = Date.ParseStrategy(
             format: "\(year: .defaultDigits)-\(month: .twoDigits)-\(day: .twoDigits)T\(hour: .defaultDigits(clock: .twentyFourHour, hourCycle: .zeroBased)):\(minute: .twoDigits):\(second: .twoDigits).\(secondFraction: .fractional(6))",
             locale: Locale(identifier: "C"),
@@ -25,6 +38,7 @@ extension Date {
             return
         }
 
+        // old Lemmy format
         let parseStrategyWithoutNanosec = Date.ParseStrategy(
             format: "\(year: .defaultDigits)-\(month: .twoDigits)-\(day: .twoDigits)T\(hour: .defaultDigits(clock: .twentyFourHour, hourCycle: .zeroBased)):\(minute: .twoDigits):\(second: .twoDigits))",
             locale: Locale(identifier: "C"),
--- Tests/LemmyKitTests/DateLemmyFormatTests.swift
+++ Tests/LemmyKitTests/DateLemmyFormatTests.swift
@@ -8,6 +8,26 @@ import XCTest
 @testable import LemmyKit
 
 final class DateLemmyFormatTests: XCTestCase {
+    func testWithNanosecLemmy0_19() throws {
+        let date = try Date(lemmyFormat: "2023-06-17T09:22:01.123456Z")
+
+        let calendar = Calendar.current
+
+        let gmt = TimeZone(secondsFromGMT: 0)!
+        let components = calendar.dateComponents(in: gmt, from: date)
+        XCTAssertEqual(components.year, 2023)
+        XCTAssertEqual(components.month, 6)
+        XCTAssertEqual(components.day, 17)
+        XCTAssertEqual(components.hour, 9)
+        XCTAssertEqual(components.minute, 22)
+        XCTAssertEqual(components.second, 1)
+        // nanosec is not what I expected, not sure what I did wrong.
+        XCTAssertEqual(components.nanosecond, 122_999_906)
+        XCTAssertEqual(components.timeZone?.identifier, "GMT")
+
+        XCTAssertEqual(date.timeIntervalSince1970, 1_686_993_721.123)
+    }
+
     func testWithNanosec() throws {
         let date = try Date(lemmyFormat: "2023-06-17T09:22:01.168808")
 
