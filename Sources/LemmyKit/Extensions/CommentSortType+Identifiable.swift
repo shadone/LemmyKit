0a25a3e13fe27c90f311e6cdbee69ae6cea2d50b
Denis Dzyubenko <denis@ddenis.info>
2024-06-16 23:07:18 +0200

    Added SortType and CommentSortType conformance to Identifiable protocol

---
+++ Sources/LemmyKit/Extensions/CommentSortType+Identifiable.swift
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2024, Denis Dzyubenko <denis@ddenis.info>
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+
+import Foundation
+
+extension Components.Schemas.CommentSortType: Identifiable {
+    public var id: String { rawValue }
+}
